13e055c6ed355e4be36e4fdc0083a6e1f39fc4e5
ibad Ahmed <ibadahmed26@gmail.com>
2024-02-19 22:28:46 +0500

    Modify: Sales flow
    feat: Internal transfer of demanding product from seller warehouse to company warehouse

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,7 +1,7 @@
 from odoo import models, api
 
 
-class StockMove(models.Model):
+class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     @api.onchange('product_id')
@@ -9,6 +9,43 @@ class StockMove(models.Model):
         for order_line in self:
             if order_line.product_id:
                 order_line.order_id.warehouse_id = order_line.product_id.warehouse.id
+
+    def action_sent_to_supplier(self):
+        for sale_line in self:
+            operational_user = self.env.user.id
+            picking_type = sale_line.warehouse_id.int_type_id
+            location_id = sale_line.warehouse_id.lot_stock_id
+            company_warehouse = self.env['stock.warehouse'].search([
+                ('company_id', '=', self.env.company.id)], limit=1)
+            seller_id = sale_line.warehouse_id.marketplace_seller_id
+            company_warehouse_location = company_warehouse.lot_stock_id
+
+            picking_vals = {
+                'partner_id': operational_user,
+                'picking_type_id': picking_type.id,
+                'location_id': location_id.id,
+                'location_dest_id': company_warehouse_location.id,
+                'seller_id': seller_id.id,
+                'move_ids_without_package': [],
+            }
+            move_line = {
+                'name': sale_line.product_id.name,
+                'product_id': sale_line.product_id.id,
+                'product_uom_qty': sale_line.product_uom_qty,
+                'product_uom': sale_line.product_id.uom_id.id,
+                'location_id': location_id.id,
+                'location_dest_id': company_warehouse_location.id,
+                'quantity_done': sale_line.product_uom_qty,
+            }
+            picking_vals['move_ids_without_package'].append((0, 0, move_line))
+
+            created_picking = self.env['stock.picking'].create(picking_vals)
+            if created_picking:
+                created_picking.action_confirm()
+                created_picking.button_validate()
+
+            if created_picking.state == 'done':
+                sale_line.write({'marketplace_state': 'sent_to_supplier'})
 
 
 class SaleOrder(models.Model):
